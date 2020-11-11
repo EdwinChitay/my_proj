d5cd8374e4d4ed29cfff067d781a3c3924c8f6c9
Edwin <edwinchitay@yahoo.com>
2020-11-10 19:07:36 -0800

    image model

--- nimbus2app/models.py
+++ nimbus2app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+import re
 # Create your models here.
+class UserManager(models.Manager):
+    def create_validator(self, reqPOST):
+        errors = {}
+        email_checker = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(reqPOST['name']) < 2:
+            errors['name'] = "Name must be at least 2 characters long"
+        if len(reqPOST['username']) < 2:
+            errors['username'] = "Username must be at least 2 characters long"
+        if not email_checker.match(reqPOST['email']):
+            errors['email'] = "Email must be valid"
+        if len(reqPOST['password']) < 8:
+            errors['password'] = "Password must be at least 8 chcaracters"
+        if reqPOST['password'] != reqPOST['confpassword']:
+            errors['password'] = "Passwords do not match!"
+        return errors
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
--- nimbus2app/views.py
+++ nimbus2app/views.py
@@ -1,5 +1,61 @@
 from django.shortcuts import render, redirect
-
+from .models import *
+from django.contrib import messages
+import bcrypt
+from .forms import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == "POST":
+        errors = User.objects.create_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/') 
+        hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        print(hashed_pw)
+        new_user = User.objects.create(name=request.POST['name'], username=request.POST['username'],
+        email=request.POST['email'], password=hashed_pw)
+        request.session['user_id'] = new_user.id
+        return redirect('/home')
+    return redirect('/') 
+
+def login(request):
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user) > 0:
+        logged_user = logged_user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            request.session['user_id'] = logged_user.id
+            return redirect('/home')
+    messages.error(request, "Email and/or password are incorrect")
+    return redirect('/')
+
+def logout(request):
+    if 'user_id' not in request.session:
+        messages.error(request, "You need to register or log in!")
+        return redirect('/')
+    request.session.clear()
+    return redirect('/')
+
+def home(request):
+    if 'user_id' not in request.session:
+        messages.error(request, "You need to register or log in!")
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['user_id']),
+    }
+    return render(request, 'home.html', context)
+
+def image_upload_view(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+            return render(request, 'home.html', {'form': form, 'img_obj': img_obj})
+        else:
+            form = ImageForm()
+        return render(request, 'home.html', {'form': form})
